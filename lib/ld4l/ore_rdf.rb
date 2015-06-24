5a4d192b4e8bc554e55a62bd8a4c9499fbfd572e
E. Lynette Rayle <elr37@cornell.edu>
2015-06-24 16:15:05 -0400

    integrate with solrizer

--- lib/ld4l/ore_rdf.rb
+++ lib/ld4l/ore_rdf.rb
@@ -1,6 +1,7 @@
 require 'rdf'
 require 'active_triples'
 require 'active_triples/local_name'
+require 'active_triples/solrizer'
 require	'linkeddata'
 require 'doubly_linked_list'
 require 'ld4l/foaf_rdf'
@@ -55,22 +56,6 @@ module LD4L
 
     autoload :CreateProxy,            'ld4l/ore_rdf/services/proxy/create'
     autoload :FindProxies,            'ld4l/ore_rdf/services/proxy/find'
-
-    def self.class_from_string(class_name, container_class=Kernel)
-      container_class = container_class.name if container_class.is_a? Module
-      container_parts = container_class.split('::')
-      (container_parts + class_name.split('::')).flatten.inject(Kernel) do |mod, class_name|
-        if mod == Kernel
-          Object.const_get(class_name)
-        elsif mod.const_defined? class_name.to_sym
-          mod.const_get(class_name)
-        else
-          container_parts.pop
-          class_from_string(class_name, container_parts.join('::'))
-        end
-      end
-    end
-
   end
 end
 
--- lib/ld4l/ore_rdf/models/aggregation_resource.rb
+++ lib/ld4l/ore_rdf/models/aggregation_resource.rb
@@ -1,97 +1,110 @@
-module LD4L
-  module OreRDF
-    class AggregationResource < ActiveTriples::Resource
+module LD4L::OreRDF
+  class AggregationResource < ActiveTriples::Resource
 
-      class << self; attr_reader :localname_prefix end
-      @localname_prefix="ag"
+    class << self; attr_reader :localname_prefix end
+    @localname_prefix="ag"
 
-      # TODO... Can we change type from Set to List to go from unordered list to ordered list?
-      # TODO    Should properties for ordered lists be blocked until the list is turned into an ordered list?
-      # TODO    Can you go from an ordered list back to an unordered list?
-      # TODO    Can you apply order to only parts of the list, or does applying order imply all items are now ordered?
-      # TODO    If all items are now ordered, what is the process for going through all subitems and adding order information?
-
-
-      # TODO... items are not persisted when this class is persisted because there is not connection from this class to the items
-      # TODO    Either the persist! method needs to be overridden to also save the items (which would also require an items attribute to hold them)
-      # TODO    OR the item property needs to be uncommented allowing the built in functionality to save them.
-      # TODO    DOWNSIDE: items and aggregators could get out of sync.  Then which is right?  Could be a problem with the first option too.
+    # TODO... Can we change type from Set to List to go from unordered list to ordered list?
+    # TODO    Should properties for ordered lists be blocked until the list is turned into an ordered list?
+    # TODO    Can you go from an ordered list back to an unordered list?
+    # TODO    Can you apply order to only parts of the list, or does applying order imply all items are now ordered?
+    # TODO    If all items are now ordered, what is the process for going through all subitems and adding order information?
 
 
-      configure :type => RDFVocabularies::ORE.Aggregation, :base_uri => LD4L::OreRDF.configuration.base_uri, :repository => :default
-
-      # extended properties for LD4L implementation
-      property :title,       :predicate => RDF::DC.title
-      property :description, :predicate => RDF::DC.description
-      property :owner,       :predicate => RDFVocabularies::DCTERMS.creator, :class_name => LD4L::FoafRDF::Person
-
-      # properties from ORE.Aggregation
-      property :aggregates,   :predicate => RDFVocabularies::ORE.aggregates, :cast => false  # multiple values
-      property :first_proxy,  :predicate => RDFVocabularies::IANA.first,     :class_name => LD4L::OreRDF::ProxyResource
-      property :last_proxy,   :predicate => RDFVocabularies::IANA.last,      :class_name => LD4L::OreRDF::ProxyResource
+    # TODO... items are not persisted when this class is persisted because there is not connection from this class to the items
+    # TODO    Either the persist! method needs to be overridden to also save the items (which would also require an items attribute to hold them)
+    # TODO    OR the item property needs to be uncommented allowing the built in functionality to save them.
+    # TODO    DOWNSIDE: items and aggregators could get out of sync.  Then which is right?  Could be a problem with the first option too.
 
 
-      # --------------------- #
-      #    HELPER METHODS     #
-      # --------------------- #
+    configure :type => RDFVocabularies::ORE.Aggregation, :base_uri => LD4L::OreRDF.configuration.base_uri, :repository => :default
+
+    # extended properties for LD4L implementation
+    property :title,       :predicate => RDF::DC.title, :indexed => true, :sortable => true
+    property :description, :predicate => RDF::DC.description, :indexed => true, :sortable => false
+    property :owner,       :predicate => RDFVocabularies::DCTERMS.creator, :class_name => LD4L::FoafRDF::Person, :indexed => true, :sortable => false
+
+    # properties from ORE.Aggregation
+    property :aggregates,   :predicate => RDFVocabularies::ORE.aggregates, :cast => false, :indexed => true, :multiple => true  # multiple values
+    property :first_proxy,  :predicate => RDFVocabularies::IANA.first,     :class_name => LD4L::OreRDF::ProxyResource
+    property :last_proxy,   :predicate => RDFVocabularies::IANA.last,      :class_name => LD4L::OreRDF::ProxyResource
 
 
-      ########### NEED TO MOVE TO SERVICE OBJECT ####################
+    # --------------------- #
+    #    HELPER METHODS     #
+    # --------------------- #
 
 
-      # Returns an array the items in the aggregation.
-      # TODO: How to begin at start and limit to number of returned items, effectively handling ranges of data.
-      def get_items_content(start=nil,limit=nil)
-        # TODO: Stubbed to return all items.
-        get_values('aggregates')
+    ########### NEED TO MOVE TO SERVICE OBJECT ####################
+
+
+    # Returns an array the items in the aggregation.
+    # TODO: How to begin at start and limit to number of returned items, effectively handling ranges of data.
+    def get_items_content(start=nil,limit=nil)
+      # TODO: Stubbed to return all items.
+      get_values('aggregates')
+    end
+
+    # Returns an array of persisted LD4L::OreRDF::ProxyResource instances for the items in the`` aggregation.
+    def get_items(start=nil,limit=nil)
+      LD4L::OreRDF::ProxyResource.get_range(self, start, limit)
+    end
+
+    # Get the persisted LD4L::OreRDF::ProxyResource instance that holds the content
+    # NOTE: Not implemented.
+    def find_item_with_content(content)
+      # TODO Stubbed - Requires definition and implementation
+    end
+
+    # Return the content at the specified position in an ordered collection
+    def get_item_content_at(position)
+      # TODO Stubbed - Required definition and implementation
+    end
+
+    # Return the LD4L::OreRDF::ProxyResource instance for the item at the specified position in an ordered collection
+    def get_item_at(position)
+      # TODO Stubbed - Required definition and implementation
+    end
+
+    # Determine if the content is already in the aggregation
+    def has_item_content?(content)
+      # TODO Stubbed - Requires definition and implementation
+    end
+
+    # Determine if an item exists at the specified position in the ordered aggregation
+    def has_item_at?(position)
+      # TODO Stubbed - Requires definition and implementation
+    end
+
+    # Destroy the item with the content and remove content from the aggregation
+    def destroy_item_with_content(content)
+      # TODO Stubbed - Requires definition and implementation
+    end
+
+    # Destroy the item at specified position and remove content from the aggregation
+    def destroy_item_at(position)
+      # TODO Stubbed - Requires definition and implementation
+    end
+
+    # Determine if the aggregation holds an ordered list of items
+    def is_ordered?
+      # TODO Stubbed to always return false until ordered lists are implemented
+      return false
+    end
+
+    def generate_solr_document
+      solr_doc = ActiveTriples::Solrizer::IndexingService.new(self).generate_solr_document do |solr_doc|
+        # TODO add owner_name and owner sort field
+        # solr_doc.merge!(:owner_name_ti => owner.name)   # TODO add name to FOAF Gem
+        # solr_doc.merge!(:owner_name_sort_ss => owner.name)
+
+        # add all item_proxies
+        all_proxies = []
+        get_items.each { |item| all_proxies << item.rdf_subject.to_s }
+        solr_doc.merge!(:item_proxies_ssm => all_proxies)
+        solr_doc
       end
-
-      # Returns an array of persisted LD4L::OreRDF::ProxyResource instances for the items in the`` aggregation.
-      def get_items(start=nil,limit=nil)
-        LD4L::OreRDF::ProxyResource.get_range(self, start, limit)
-      end
-
-      # Get the persisted LD4L::OreRDF::ProxyResource instance that holds the content
-      # NOTE: Not implemented.
-      def find_item_with_content(content)
-        # TODO Stubbed - Requires definition and implementation
-      end
-
-      # Return the content at the specified position in an ordered collection
-      def get_item_content_at(position)
-        # TODO Stubbed - Required definition and implementation
-      end
-
-      # Return the LD4L::OreRDF::ProxyResource instance for the item at the specified position in an ordered collection
-      def get_item_at(position)
-        # TODO Stubbed - Required definition and implementation
-      end
-
-      # Determine if the content is already in the aggregation
-      def has_item_content?(content)
-        # TODO Stubbed - Requires definition and implementation
-      end
-
-      # Determine if an item exists at the specified position in the ordered aggregation
-      def has_item_at?(position)
-        # TODO Stubbed - Requires definition and implementation
-      end
-
-      # Destroy the item with the content and remove content from the aggregation
-      def destroy_item_with_content(content)
-        # TODO Stubbed - Requires definition and implementation
-      end
-
-      # Destroy the item at specified position and remove content from the aggregation
-      def destroy_item_at(position)
-        # TODO Stubbed - Requires definition and implementation
-      end
-
-      # Determine if the aggregation holds an ordered list of items
-      def is_ordered?
-        # TODO Stubbed to always return false until ordered lists are implemented
-        return false
-      end
+      solr_doc
     end
   end
 end
